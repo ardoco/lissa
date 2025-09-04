9ccf1a7d57ed652c2fbe9ac9cb04be8c36b319ea
Tobias Thirolf <ulwtb@student.kit.edu>
2025-09-04 18:28:30 +0200

    optimize analysis format

--- src/main/java/edu/kit/kastel/sdq/lissa/ratlr/Analysis.java
+++ src/main/java/edu/kit/kastel/sdq/lissa/ratlr/Analysis.java
@@ -2,55 +2,64 @@ package edu.kit.kastel.sdq.lissa.ratlr;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import edu.kit.kastel.mcse.ardoco.metrics.result.SingleClassificationResult;
+import edu.kit.kastel.sdq.lissa.ratlr.cache.CacheKey;
 import edu.kit.kastel.sdq.lissa.ratlr.cache.CacheManager;
 import edu.kit.kastel.sdq.lissa.ratlr.classifier.ClassificationResult;
-import edu.kit.kastel.sdq.lissa.ratlr.classifier.Classifier;
 import edu.kit.kastel.sdq.lissa.ratlr.knowledge.Element;
 import edu.kit.kastel.sdq.lissa.ratlr.knowledge.TraceLink;
 import edu.kit.kastel.sdq.lissa.ratlr.postprocessor.TraceLinkIdPostprocessor;
 import edu.kit.kastel.sdq.lissa.ratlr.utils.Pair;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Analysis {
 
     @JsonProperty
-    private final List<ElementAnalysis> sourceElements;
+    private final List<Map<String, Object>> sourceElements;
     @JsonProperty
-    private final List<ElementAnalysis> targetElements;
+    private final List<Map<String, Object>> targetElements;
     @JsonProperty
-    private final List<ClassificationResultAnalysis> truePositives = new LinkedList<>();
+    private final Map<String, Map<String, String>> truePositives = new LinkedHashMap<>();
     @JsonProperty
-    private final List<ClassificationResultAnalysis> falseNegatives = new LinkedList<>();
+    private final Map<String, Map<String, String>> falseNegatives = new LinkedHashMap<>();
     @JsonProperty
-    private final List<ClassificationResultAnalysis> falsePositives = new LinkedList<>();
+    private final Map<String, Map<String, String>> falsePositives = new LinkedHashMap<>();
     @JsonProperty
-    private final List<ClassificationResultAnalysis> trueNegatives = new LinkedList<>();
+    private final Map<String, Map<String, String>> trueNegatives = new LinkedHashMap<>();
 
     public Analysis(List<Element> sourceElements, List<Element> targetElements, List<Pair<Element, Element>> retrieved, 
                     List<ClassificationResult> llmResults, SingleClassificationResult<TraceLink> statistics, TraceLinkIdPostprocessor postprocessor) {
         this.sourceElements = sourceElements.stream()
                 .filter(element -> element.getParent() == null)
-                .map(element -> new ElementAnalysis(element, sourceElements, true))
+                .map(element -> {
+                    Map<String, Object> map = new LinkedHashMap<>();
+                    map.put(element.getIdentifier(), getElementMap(element, sourceElements, true));
+                    return map;
+                })
                 .toList();
         this.targetElements = targetElements.stream()
                 .filter(element -> element.getParent() == null)
-                .map(element -> new ElementAnalysis(element, targetElements, true))
+                .map(element -> {
+                    Map<String, Object> map = new LinkedHashMap<>();
+                    map.put(element.getIdentifier(), getElementMap(element, targetElements, true));
+                    return map;
+                })
                 .toList();
         for (Pair<Element, Element> retrievedPair : retrieved) {
-            ClassificationResultAnalysis analysis = new ClassificationResultAnalysis(retrievedPair.first().getIdentifier(), retrievedPair.second().getIdentifier(), 
-                    CacheManager.getDefaultInstance().getKey(retrievedPair.first(), retrievedPair.second()).localKey());
+            CacheKey key = CacheManager.getDefaultInstance().getKey(retrievedPair.first(), retrievedPair.second());
             TraceLink retrievedProcessed = postprocessor.postprocess(Set.of(new TraceLink(retrievedPair.first().getIdentifier(), retrievedPair.second().getIdentifier())))
                     .iterator().next();
-            getConfusionCollection(statistics, retrievedProcessed).add(analysis);
+            Map<String, Map<String, String>> confusionCollection = getConfusionCollection(statistics, retrievedProcessed);
+            confusionCollection.putIfAbsent(retrievedPair.first().getIdentifier(), new LinkedHashMap<>());
+            confusionCollection.get(retrievedPair.first().getIdentifier()).put(retrievedPair.second().getIdentifier(), key == null ? null : key.localKey());
         }
     }
 
-    private List<ClassificationResultAnalysis> getConfusionCollection(SingleClassificationResult<TraceLink> statistics, TraceLink retrievedProcessed) {
-        List<ClassificationResultAnalysis> collector = null;
+    private Map<String, Map<String, String>> getConfusionCollection(SingleClassificationResult<TraceLink> statistics, TraceLink retrievedProcessed) {
+        Map<String, Map<String, String>> collector = null;
         for (TraceLink truePositive : statistics.getTruePositives()) {
             if (retrievedProcessed.sourceId().equals(truePositive.sourceId()) && retrievedProcessed.targetId().equals(truePositive.targetId())) {
                 collector = this.truePositives;
@@ -79,55 +88,17 @@ public class Analysis {
         return collector;
     }
 
-    private static final class ClassificationResultAnalysis {
-        @JsonProperty
-        private final String sourceIdentifier;
-        @JsonProperty
-        private final String targetIdentifier;
-        @JsonProperty
-        private final String responseCacheKey;
-
-        public ClassificationResultAnalysis(String sourceIdentifier, String targetIdentifier, String responseCacheKey) {
-            this.sourceIdentifier = sourceIdentifier;
-            this.targetIdentifier = targetIdentifier;
-            this.responseCacheKey = responseCacheKey;
-        }
-    }
-    
-    private static final class ElementAnalysis {
-        /** The unique identifier of this knowledge unit */
-        @JsonProperty
-        private final String identifier;
-        /** The type of this knowledge unit */
-        @JsonProperty
-        private final String type;
-        /** The original content of this knowledge unit */
-        @JsonProperty
-        private final String content;
-        /** The granularity level of this element, indicating its level of detail */
-        @JsonProperty
-        private final int granularity;
-        /** The parent element of this element, if any */
-        @JsonProperty
-        private final List<ElementAnalysis> children = new ArrayList<>();
-        /**
-         *  Flag indicating whether this element should be included in comparisons.
-         *  {@link Classifier Classifiers} will only consider this element for candidate pairs for classification if this is true.
-         */
-        @JsonProperty
-        private final boolean compare;
-
-        public ElementAnalysis(Element element, List<Element> allElements, boolean root) {
-            this.identifier = element.getIdentifier();
-            this.type = element.getType();
-            this.content = root ? "root" : element.getContent();
-            this.granularity = element.getGranularity();
-            this.compare = element.isCompare();
-            for (Element allElement : allElements) {
-                if (element.equals(allElement.getParent())) {
-                    this.children.add(new ElementAnalysis(allElement, allElements, false));
-                }
+    private static Map<String, Object> getElementMap(Element element, List<Element> allElements, boolean root) {
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("type", element.getType());
+        map.put("content", root ? "root" : element.getContent());
+        map.put("granularity", element.getGranularity());
+        map.put("compare", element.isCompare());
+        for (Element allElement : allElements) {
+            if (element.equals(allElement.getParent())) {
+                map.put(allElement.getIdentifier(), getElementMap(allElement, allElements, false));
             }
         }
+        return map;
     }
 }
