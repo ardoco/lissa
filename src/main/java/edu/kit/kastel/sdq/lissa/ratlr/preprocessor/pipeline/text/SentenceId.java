8ec2037daccc72f0b433ea7738ece4735b371d29
Tobias Thirolf <ulwtb@student.kit.edu>
2025-10-07 14:34:38 +0200

    add line identifiers

---
+++ src/main/java/edu/kit/kastel/sdq/lissa/ratlr/preprocessor/pipeline/text/SentenceId.java
@@ -0,0 +1,29 @@
+package edu.kit.kastel.sdq.lissa.ratlr.preprocessor.pipeline.text;
+
+import edu.kit.kastel.sdq.lissa.ratlr.context.ContextStore;
+import edu.kit.kastel.sdq.lissa.ratlr.knowledge.Element;
+import edu.kit.kastel.sdq.lissa.ratlr.preprocessor.pipeline.SingleElementProcessingStage;
+
+import java.util.List;
+import java.util.StringJoiner;
+
+public class SentenceId extends SingleElementProcessingStage {
+    /**
+     * Creates a new preprocessor with the specified context store.
+     *
+     * @param contextStore The shared context store for pipeline components
+     */
+    public SentenceId(ContextStore contextStore) {
+        super(contextStore);
+    }
+
+    @Override
+    protected List<Element> process(Element element) {
+        String[] lines = element.getContent().split("\n");
+        StringJoiner joiner = new StringJoiner("\n");
+        for (int i = 0; i < lines.length; i++) {
+            joiner.add((i + 1) + ": " + lines[i]);
+        }
+        return List.of(Element.fromParent(element, joiner.toString()));
+    }
+}
