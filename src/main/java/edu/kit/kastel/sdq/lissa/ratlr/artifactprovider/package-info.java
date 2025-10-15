205b603b82d1d83ccf0696d4d2f3750b698b816a
Dominik Fuchß <dominik.fuchss@kit.edu>
2025-10-15 15:45:27 +0200

    Remove package-info.java

--- src/main/java/edu/kit/kastel/sdq/lissa/ratlr/artifactprovider/package-info.java
+++
@@ -1,2 +0,0 @@
-@org.jspecify.annotations.NullMarked
-package edu.kit.kastel.sdq.lissa.ratlr.artifactprovider;
--- src/main/java/edu/kit/kastel/sdq/lissa/ratlr/elementstore/strategy/package-info.java
+++
@@ -1,2 +0,0 @@
-@org.jspecify.annotations.NullMarked
-package edu.kit.kastel.sdq.lissa.ratlr.elementstore.strategy;
